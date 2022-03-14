326ee770a2129bca95293bd2c00ea75883c0b5b1
Belen Nazareth <belennazareth29@gmail.com>
2022-03-14 19:34:58 +0100

    Actualización del esquema de script básico

--- ISO/Esquemas_scripts.sh
+++ ISO/Esquemas_scripts.sh
@@ -150,66 +150,69 @@
 «»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»««»«»«»«»«»«»«»«»»«»«»«»«»«»«»«»»«»««»«»«»«»
 
     #  **SEXTA MODIFICACIÓN DE LA PLANTILLA**
-        
+        #Hacemos que se compruebe si el fichero existe o no,
+        #si ya existe lanzará un mensaje.
+
         #nano plantillafinal.sh
 
-        #!/usr/bin/env bash
-        #zona de declaracion de variables
-        ruta="/home/usuario/script/scripts"
+            #!/usr/bin/env bash
+            #zona de declaracion de variables
+            ruta="/home/usuario/script/scripts"
 
-        #fin zona
+            #fin zona
 
-        #zona declaracion de funciones
+            #zona declaracion de funciones
 
-        #fin zona
+            #fin zona
 
-        echo "Introduce el nombre del fichero con extensión .sh: "
-        read nombrefich 
-        if [ -f $ruta$nombrefich ];then
-            echo "$nombrefich ya existe"
-            exit
-        fi
+            echo "Introduce el nombre del fichero con extensión .sh: "
+            read nombrefich 
+            if [ -f $ruta$nombrefich ];then
+                echo "$nombrefich ya existe"
+                exit
+            fi
 
-        echo "#!/usr/bin/env bash" > $ruta$nombrefich
-        echo "#Autor: $USER" >> $ruta$nombrefich
-        echo "#Versión: " >> $ruta$nombrefich
-        echo "#Descripción: " >> $ruta$nombrefich
-        echo "#Fecha de creación: $(date)" >> $ruta$nombrefich
-        echo "#Fecha de modificación: " >> $ruta$nombrefich
-        chmod a+x $ruta$nombrefich
+            echo "#!/usr/bin/env bash" > $ruta$nombrefich
+            echo "#Autor: $USER" >> $ruta$nombrefich
+            echo "#Versión: " >> $ruta$nombrefich
+            echo "#Descripción: " >> $ruta$nombrefich
+            echo "#Fecha de creación: $(date)" >> $ruta$nombrefich
+            echo "#Fecha de modificación: " >> $ruta$nombrefich
+            chmod a+x $ruta$nombrefich
 
         # ./plantillafinal.sh 
 
 «»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»«»««»«»«»«»«»«»«»«»»«»«»«»«»«»«»«»»«»««»«»«»«»
 
     #  **SÉPTIMA MODIFICACIÓN DE LA PLANTILLA**
+        #Añadida una expresión que nos indica la versión de forma automática.
 
         #nano plantillafinal.sh
 
-        #!/usr/bin/env bash
-        #zona de declaracion de variables
-        ruta="/home/usuario/script/scripts"
-
-        #fin zona
-
-        #zona declaracion de funciones
-
-        #fin zona
-
-        echo "Introduce el nombre del fichero con extensión .sh: "
-        read nombrefich 
-        if [ -f $ruta$nombrefich ];then
-            echo "$nombrefich ya existe"
-            exit
-        fi
-
-        echo "#!/usr/bin/env bash" > $ruta$nombrefich
-        echo "#Autor: $USER" >> $ruta$nombrefich
-        echo "#Versión: $(bash --version)" >> $ruta$nombrefich
-        echo "#Descripción: " >> $ruta$nombrefich
-        echo "#Fecha de creación: $(date)" >> $ruta$nombrefich
-        echo "#Fecha de modificación: " >> $ruta$nombrefich
-        chmod a+x $ruta$nombrefich
+            #!/usr/bin/env bash
+            #zona de declaracion de variables
+            ruta="/home/usuario/script/scripts"
+    
+            #fin zona
+    
+            #zona declaracion de funciones
+    
+            #fin zona
+    
+            echo "Introduce el nombre del fichero con extensión .sh: "
+            read nombrefich 
+            if [ -f $ruta$nombrefich ];then
+                echo "$nombrefich ya existe"
+                exit
+            fi
+    
+            echo "#!/usr/bin/env bash" > $ruta$nombrefich
+            echo "#Autor: $USER" >> $ruta$nombrefich
+            echo "#Versión: $(bash --version | egrep 'GNU bash')" >> $ruta$nombrefich
+            echo "#Descripción: " >> $ruta$nombrefich
+            echo "#Fecha de creación: $(date)" >> $ruta$nombrefich
+            echo "#Fecha de modificación: " >> $ruta$nombrefich
+            chmod a+x $ruta$nombrefich
 
         # ./plantillafinal.sh 
 
