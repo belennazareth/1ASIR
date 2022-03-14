9eaef0c59507b73a8ce04cc86d6564dd11c17cda
Belen Nazareth <belennazareth29@gmail.com>
2022-03-14 14:18:35 +0100

    Actualizado esquemas para hacer plantillas de scripts

--- ISO/Esquemas_scripts.sh
+++ ISO/Esquemas_scripts.sh
@@ -164,3 +164,35 @@ nano plantillafinal.sh
     chmod a+x $ruta$nombrefich
 
 ./plantillafinal.sh (para ejecutarlo)
+
+
+#  **OCTAVA PLANTILLA**
+
+nano plantillafinal.sh
+
+    #!/usr/bin/env bash
+    #zona de declaracion de variables
+    ruta="/home/usuario/script/scripts"
+
+    #fin zona
+
+    #zona declaracion de funciones
+
+    #fin zona
+
+    echo "Introduce el nombre del fichero con extensión .sh: "
+    read nombrefich (con esto podemos esperar a que se meta algo)
+    if [ -f $ruta$nombrefich ];then
+        echo "$nombrefich ya existe"
+        exit
+    fi
+
+    echo "#!/usr/bin/env bash" > $ruta$nombrefich
+    echo "#Autor: $USER" >> $ruta$nombrefich
+    echo "#Versión: $(bash --version)" >> $ruta$nombrefich
+    echo "#Descripción: " >> $ruta$nombrefich
+    echo "#Fecha de creación: $(date)" >> $ruta$nombrefich
+    echo "#Fecha de modificación: " >> $ruta$nombrefich
+    chmod a+x $ruta$nombrefich
+
+./plantillafinal.sh (para ejecutarlo)
