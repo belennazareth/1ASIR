f530f1ebb69c7637036486acb84ece6f5107488b
Belen Nazareth <belennazareth29@gmail.com>
2022-03-16 09:45:21 +0100

    Creación de un nuevo script

---
+++ ISO/script1.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Descripción: ...
+#             Necesito de 3 argumentos,
+#                1. un directorio ...
+
+#Zona de declaración de funciones:
+#Fin zona
+#Zona de declaración de variables:
+contador=0
+indice=0
+ruta="/home/usuario"
+#Fin zona
